f664bcf894a52397e51dccc1a2e638c65ac03c8b
Sethulakshmi <sethulakshmi.pm@razorthink.com>
2019-04-02 16:49:46 +0530

    learnJava-FirstCommit

---
+++ FirstJavaProject/src/com/rzt/first/MyFirstClass.java
@@ -0,0 +1,10 @@
+package com.rzt.first;
+
+/**
+ * Created by SethulakshmiPM on 4/2/19
+ **/
+public class MyFirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello-SETHU");
+    }
+}
