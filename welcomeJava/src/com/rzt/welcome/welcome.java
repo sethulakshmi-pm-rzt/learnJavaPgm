d6c1143e291b806d3a1961536b5ce83d438302bd
Sethulakshmi <sethulakshmi.pm@razorthink.com>
2019-04-02 17:33:23 +0530

    first commit

---
+++ welcomeJava/src/com/rzt/welcome/welcome.java
@@ -0,0 +1,10 @@
+package com.rzt.welcome;
+
+/**
+ * Created by SethulakshmiPM on 4/2/19
+ **/
+public class welcome {
+    public static void main(String[] args) {
+        System.out.println("welcomeJava --");
+    }
+}
