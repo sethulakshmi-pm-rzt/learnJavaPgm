ed7b94bb05ff7b995863439674e7cc562c732bcb
Sethulakshmi <sethulakshmi.pm@razorthink.com>
2019-04-02 18:14:49 +0530

    changeS

---
+++ welcomeJava/src/com/rzt/welcome/exampleDoWhile.java
@@ -0,0 +1,16 @@
+package com.rzt.welcome;
+
+/**
+ * Created by SethulakshmiPM on 4/2/19
+ **/
+
+// Example for "DoWhile loop"
+public class exampleDoWhile {
+    public static void main(String[] args) {
+        int x = 10;
+        do {
+            System.out.println("DO" + x);
+            x--;
+        } while (x > 5);
+    }
+}
